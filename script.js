68b4c78486a7d4db5bccd577ec734b5f6d393740
Guilherme Guimarães Scherengovski <114667943+guilherme-sherenga@users.noreply.github.com>
2026-03-22 17:49:00 -0300

    Adicionados comentários ao JS

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
+// Aqui, seleciona o formulário pelo ID formContato e adiciona um evento de envio (submit)
 document.getElementById("formContato").addEventListener("submit", function(e) {
+  // Adicionado evento para impedir o comportamento padrão do formulário (recarregar a página)
   e.preventDefault();
 
+  // Captura os campo "nome", "e-mail" e "mensagem" removendo espaços extras
   let nome = document.getElementById("nome").value.trim();
   let email = document.getElementById("email").value.trim();
   let mensagem = document.getElementById("mensagem").value.trim();
 
+  // Aqui, verifica se há algum espaço vazio. Se sim, emite mensagem e retorna para preenchimento
   if (nome === "" || email === "" || mensagem === "") {
     alert("Preencha todos os campos!");
     return;
   }
 
+  // Validação simples de e-mail que avisa caso esqueça algum caractere
   if (!email.includes("@") || !email.includes(".")) {
     alert("Email inválido!");
     return;
   }
 
+  // Se tudo estiver correto, mostra mensagem de envio
   alert("Mensagem enviada com sucesso!");
 
+  // Limpa o formulário após envio
   this.reset();
 });
